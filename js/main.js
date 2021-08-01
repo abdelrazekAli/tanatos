258107b25806b0902c6b6f7e0b590355f837d11a
Dev-Abdelrazek <abdelrazek.ali.abdelrazek@gmail.com>
2021-08-01 07:01:16 +0200

    Update design

--- js/main.js
+++ js/main.js
@@ -1,34 +1,17 @@
+//Adjust Slider Height
 $(function () {
-  'use strict';
-
-  //Adjust Slider Height
-  //scroll to skills
-  $('.header .content .icon2').click(function(){
-    $('html, body').animate({
-      scrollTop: $('.skills').offset().top
-    },1000);
-  });
-  $('.header .hire-us').click(function(){
-    $('html, body').animate({
-      scrollTop: $('.say-hello').offset().top
-    },1000);
-  });
-  $('.header .our-works').click(function(){
-    $('html, body').animate({
-      scrollTop: $('#works').offset().top
-    },1000);
-  });
+  $(".header").height($(window).height());
 });
-//show button
+
+//show and hidden button
 let hiddenDiv = document.getElementById("hiddenDiv"),
-    btn = document.getElementById("btn");
-btn.onclick = function(){
-  if(btn.textContent == "show more"){
+  btn = document.getElementById("btn");
+btn.onclick = function () {
+  if (btn.textContent == "show more") {
     btn.innerText = "show less";
     hiddenDiv.classList.toggle("hidden");
-  }
-  else{
+  } else {
     btn.innerText = "show more";
     hiddenDiv.classList.toggle("hidden");
   }
-}
+};
